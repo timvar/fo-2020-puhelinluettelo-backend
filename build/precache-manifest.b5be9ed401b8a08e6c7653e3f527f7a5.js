0507535bb282c7a71d2ce31056b08e9c99f877fa
Timo Varila <timo.varila@siili.com>
2020-04-07 15:55:26 +0300

    added frontend build directory

---
+++ build/precache-manifest.b5be9ed401b8a08e6c7653e3f527f7a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb34d800ad77d2b3d7e56e206451225",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84c812d52a7b2c4ffda2",
+    "url": "/static/css/main.72ce75d3.chunk.css"
+  },
+  {
+    "revision": "08d3aad3ecae6194104c",
+    "url": "/static/js/2.f518c3b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f518c3b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84c812d52a7b2c4ffda2",
+    "url": "/static/js/main.4d87cac7.chunk.js"
+  },
+  {
+    "revision": "c940798f285500efd361",
+    "url": "/static/js/runtime-main.64749484.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body)})
 
