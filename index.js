fe05fe7b86e6fa61d80372c58fe6156835423928
Timo Varila <timo.varila@siili.com>
2020-04-07 15:04:58 +0300

    step9

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body)})
@@ -87,7 +90,7 @@ let persons = [
     res.status(204).end()
   })
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
 
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
