9ee993dc4777da38a96fbf06e53f62606707f984
Timo Varila <timo.varila@siili.com>
2020-04-08 15:31:32 +0300

    puhelinluettelo ja tietokanta, step4

--- index.js
+++ index.js
@@ -6,43 +6,35 @@ const Person = require('./models/person')
 
 const app = express()
 app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
-let persons = []
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res,next) => {
   Person.find({})
-    .then(persons => {
-      res.json(persons.map(person => person.toJSON()));
-    })
+  .then(persons => {
+    res.json(persons.map(person => person.toJSON()));
+  })
+  .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
-    .then(person => {
-      if (person) {
-        res.json(person.toJSON())
-      } else {
-        res.status(404).end()
-      }
-    })
-    .catch(error => {
-      console.log(error)
-      res.status(400).send({ error: 'malformatted id' })
-    })
+  .then(person => {
+    if (person) {
+      res.json(person.toJSON())
+    } else {
+      res.status(404).end()
+    }
+  })
+  .catch(error => next(error))
 })
 
-app.get('/info', (req, res) => {
-  let info = `<p>Phonebook has info for ${persons.length} people<p> <p>${new Date()}<p>`
-  res.send(info);
-})
-
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
-
+  
   if (!body.name) {
     return res.status(400).json({
       error: 'name missing'
@@ -52,24 +44,40 @@ app.post('/api/persons', (req, res) => {
       error: 'number missing'
     })
   }
-
+  
   const person = new Person({
     name: body.name,
     number: body.number
   })
-
-  person.save().then(person => {
+  
+  person.save()
+  .then(person => {
     res.json(person.toJSON());
   })
+  .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-    .then(result => {
-      res.status(204).end()
-    })
-    .catch(error => next(error))
+  .then(result => {
+    res.status(204).end()
+  })
+  .catch(error => next(error))
 })
+
+const unknownEndPoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndPoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
