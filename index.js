1fcd8ccb8a6914169b0c62ef1a40cb94b55d282f
Timo Varila <timo.varila@siili.com>
2020-04-08 15:49:07 +0300

    3.17 puhelinluettelo ja tietokanta step5

--- index.js
+++ index.js
@@ -12,29 +12,29 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
 
-app.get('/api/persons', (req, res,next) => {
+app.get('/api/persons', (req, res, next) => {
   Person.find({})
-  .then(persons => {
-    res.json(persons.map(person => person.toJSON()));
-  })
-  .catch(error => next(error))
+    .then(persons => {
+      res.json(persons.map(person => person.toJSON()));
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
-  .then(person => {
-    if (person) {
-      res.json(person.toJSON())
-    } else {
-      res.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+    .then(person => {
+      if (person) {
+        res.json(person.toJSON())
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
   const body = req.body
-  
+
   if (!body.name) {
     return res.status(400).json({
       error: 'name missing'
@@ -44,25 +44,38 @@ app.post('/api/persons', (req, res, next) => {
       error: 'number missing'
     })
   }
-  
+
   const person = new Person({
     name: body.name,
     number: body.number
   })
-  
+
   person.save()
-  .then(person => {
-    res.json(person.toJSON());
+    .then(person => {
+      res.json(person.toJSON());
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const person = {
+    name: req.body.name,
+    number: req.body.number
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+  .then(updatedPerson => {
+    res.json(updatedPerson.toJSON())
   })
   .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-  .then(result => {
-    res.status(204).end()
-  })
-  .catch(error => next(error))
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndPoint = (req, res) => {
